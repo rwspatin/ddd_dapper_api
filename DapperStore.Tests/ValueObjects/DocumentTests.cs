2391d33e317645bd95743c4edc8bd51cfd41287a
Renan Winter Spatin <rwspatin@gmail.com>
2020-07-21 15:12:21 -0300

    Create ValueObject Test

---
+++ DapperStore.Tests/ValueObjects/DocumentTests.cs
@@ -0,0 +1,29 @@
+using DapperStore.Domain.StoreContext.ValueObjects;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DapperStore.Tests.ValueObjects
+{
+    [TestClass]
+    public class DocumentTests
+    {
+        private Document validDocument;
+        private Document invalidDocument;
+        public DocumentTests(){
+            invalidDocument = new Document("121313131");
+            validDocument = new Document("121313131");
+        }
+        [TestMethod]
+        public void ShouldReturnNotificationWhenDocumentIsNotValid()
+        {
+            Assert.IsFalse(invalidDocument.Valid);
+            Assert.IsTrue(invalidDocument.Notifications.Count >= 1);
+        }
+
+        [TestMethod]
+        public void ShouldReturnNotNotificationWhenDocumentIsValid()
+        {
+            Assert.IsTrue(validDocument.Valid);
+            Assert.AreEqual(0, validDocument.Notifications.Count);
+        }
+    }
+}
